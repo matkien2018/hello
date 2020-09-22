8f78537774439af2f487a8513dc35f213c4a091c
Binh Le <binhle@Binh-MacBookPro.local>
2020-09-22 08:20:10 -0400

    add more file from other laptop and so we can manually sync

---
+++ debug_sample.py
@@ -0,0 +1,5 @@
+def print_n(x):
+    for i in range(1,x+1):
+        print(i)
+
+print_n(10)
--- first_turtle.py
+++ first_turtle.py
@@ -1,5 +1,17 @@
 import turtle
 """ https://docs.python.org/3/library/turtle.html#turtle.pen """
-t = turtle.Turtle
+w = turtle.Screen()
+w.bgcolor("lightgreen")
+t = turtle.Turtle()
+t.shape("turtle")
+t.color("blue")
 
-t.pen(pencolor="purple", fillcolor="#99cfe0", pensize=10, speed=2)
+t.penup()
+size = 20
+for i in range(30):
+    t.stamp()
+    size = size + 3
+    t.forward(size)
+    t.right(24)
+#t.pen(pencolor="purple", fillcolor="#99cfe0", pensize=10, speed=2)
+w.mainloop()
